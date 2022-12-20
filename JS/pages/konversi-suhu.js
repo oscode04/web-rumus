87f503d0ba3e81390d326f96c602c9f22485be05
gaos <hellogaos@gmail.com>
2022-12-20 19:12:56 +0700

    nambah halaman donasi, baru mulai bikin halaman convert suhu dan perbaiki html dikit

---
+++ JS/pages/konversi-suhu.js
@@ -0,0 +1,12 @@
+function hitungSuhu() {
+  let celcius1 = document.getElementById("celcius1").checked;
+  let fahrenheit1 = document.getElementById("fahrenheit1").checked;
+
+  if (celcius1 == true) {
+    console.log("ok");
+  } else {
+    console.log("note ok");
+  }
+
+  console.log();
+}
